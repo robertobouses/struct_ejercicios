1562569fffeaef29073b9ecb5b61444e4e063e71
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-05-22 18:09:48 +0200

    Persona

--- main.go
+++ main.go
@@ -2,4 +2,26 @@ package main
 
 import "fmt"
 
-func main() { fmt.Println("viejo") }
+type Persona struct {
+	nombre string
+	edad   int
+}
+
+func (per *Persona) Scanear() {
+	fmt.Println("Introduce los datos de la persona, nombre y edad:")
+	fmt.Scanln(&per.nombre)
+	fmt.Scanln(&per.edad)
+}
+
+func (per Persona) Imprimir() {
+	fmt.Println("Se llama", per.nombre)
+	fmt.Println("Tiene", per.edad, "años")
+}
+
+func main() {
+	fmt.Println("Viejo")
+
+	var Persona1 Persona
+	Persona1.Scanear()
+	Persona1.Imprimir()
+}
